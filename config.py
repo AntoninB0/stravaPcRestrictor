62e4af5305ab1d651a436719a2b9d1449c13c1b3
Antonin <bo.antonin@gmail.com>
2024-02-15 01:49:22 +0100

    arangement

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+url = 'https://www.strava.com/api/v3/'
+authorization_url = 'https://www.strava.com/oauth/authorize'
+redirect_uri = 'http://localhost:8080/callback'
+scope = 'read_all,activity:read_all'
+
 strava_id = "xxxxx" 
 strava_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 email = 'xxxxxxxxxxxx@gmail.com'
--- main.py
+++ main.py
@@ -1,58 +1,55 @@
-from config import strava_id, strava_secret ,email , password
-import requests
+from config import url, authorization_url, redirect_uri, scope, strava_id, strava_secret ,email , password
 from urllib.parse import urlencode
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+import requests
 from selenium.webdriver.common.by import By
-import json
+from selenium import webdriver
 import time
 
-url = 'https://www.strava.com/api/v3/'
-authorization_url = 'https://www.strava.com/oauth/authorize'
-redirect_uri = 'http://localhost:8080/callback'
-scope = 'read_all,activity:read_all'
+def scrapping(authorize_url):
 
-authorize_params = {
-    'client_id': strava_id,
-    'redirect_uri': redirect_uri,
-    'response_type': 'code',
-    'scope': scope,
-}
+    driver = webdriver.Firefox()
+    driver.get(authorize_url)
 
-authorize_url = f'{authorization_url}?{urlencode(authorize_params)}'
-print(f'Authorize the app by visiting the following link and copy the authorization code from the URL:\n{authorize_url}')
+    driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(email)
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+    driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    driver.find_element(By.XPATH, '//*[@id="authorize"]').click()
 
-################################################
+    time.sleep(10)
+    redirected_url = driver.current_url
+    driver.quit()
+    return redirected_url.split("&")[1].replace("code=","")
 
-driver = webdriver.Firefox()
-driver.get(authorize_url)
+def loginApi():
 
-driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(email)
-driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
-driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
-driver.find_element(By.XPATH, '//*[@id="authorize"]').click()
+    authorize_params = {
+        'client_id': strava_id,
+        'redirect_uri': redirect_uri,
+        'response_type': 'code',
+        'scope': scope,
+    }
 
-time.sleep(10)
-redirected_url = driver.current_url
-driver.quit()
+    authorize_url = f'{authorization_url}?{urlencode(authorize_params)}'
 
-################################################
+    authorization_code = scrapping(authorize_url)
+    print(authorization_code)
+
+    token_url = 'https://www.strava.com/oauth/token'
+    token_params = {
+        'client_id': strava_id,
+        'client_secret': strava_secret,
+        'code': authorization_code,
+        'grant_type': 'authorization_code',
+    }
+
+    token_response = requests.post(token_url, data=token_params)
+    access_token = token_response.json()['access_token']
+
+    return access_token
 
 
-print(redirected_url.split("&")[1].replace("code=",""))
 
-authorization_code = redirected_url.split("&")[1].replace("code=","")
-
-token_url = 'https://www.strava.com/oauth/token'
-token_params = {
-    'client_id': strava_id,
-    'client_secret': strava_secret,
-    'code': authorization_code,
-    'grant_type': 'authorization_code',
-}
-
-token_response = requests.post(token_url, data=token_params)
-access_token = token_response.json()['access_token']
+access_token = loginApi()
 
 activities_url = f'{url}athlete/activities'
 headers = {'Authorization': f'Bearer {access_token}'}
